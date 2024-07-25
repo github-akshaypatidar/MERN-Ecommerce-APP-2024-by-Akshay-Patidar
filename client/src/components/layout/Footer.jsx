9c784f730645ae9db34c9322e42728a2395c7478
Akshay Patidar <akshayedumail@gmail.com>
2024-07-25 16:37:43 +0530

    modified:   client/src/components/layout/Footer.jsx 	modified:   client/src/components/layout/Header.jsx 	modified:   client/src/index.css

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
+  const date= new Date();
+  const year= date.getFullYear();
   return (
     <div className='bg-dark text-light p-3 '>
-     <h4 className='text-center'> All Rights Reserved &copy; Akshay Patidar</h4>
+     <h5 className='text-center'> All Rights Reserved &copy; {year} <NavLink target='_blank' style={{textDecoration:'none', color:'#fff'}} to='https://www.linkedin.com/in/dev-akshaypatidar/'>Akshay Patidar</NavLink></h5>
       </div>
   )
 }
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#"> BrandCommerce</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon" />
   </button>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ms-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      <li className="nav-item ">
+      <NavLink className='nav-link' to='/'>Home</NavLink>  
+     
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+      <li className="nav-item ">
+      <NavLink className='nav-link' to='/register'>Register</NavLink>  
       </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider" />
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
+     
+      <li className="nav-item ">
+      <NavLink className='nav-link' to='/login'>Login</NavLink>  
+     
+      </li>
+      <li className="nav-item ">
+      <NavLink className='nav-link' to='/cart'>Cart (0)</NavLink>  
+     
       </li>
     
     </ul>
